b1d4cbb2211bf7ffa6f9832bb5dfc8837e32214f
Ömer Sezer <oomersezer@gmail.com>
2018-08-22 17:39:03 +0300

    Örnek proje tamamlandı

---
+++ Popup/Popup/Popup/AnaSayfa.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Popup
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AnaSayfa : ContentPage
+	{
+		public AnaSayfa ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            PopupNavigation.Instance.PushAsync(new MainPage());
+        }
+    }
+}
